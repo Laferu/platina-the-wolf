cbb7173e86e0fedc43803cbb59572fb183f94ad9
Laferu <rp.araujo2@gmail.com>
2020-03-23 10:52:59 -0300

    Correção quinta tentativa

---
+++ _next/static/bF31A07IXaA3Z0LCflImq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/platina-the-wolf-and-the-heir-of-light":[a]}}("static\u002Fchunks\u002F1a791854c7f068b4afd80482f3cafd46f5f951f1.0d9c510252388f291055.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
