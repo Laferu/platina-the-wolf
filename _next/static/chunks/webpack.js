41d4bc3b55db03c5ae1d8ee1a5611fd56040a003
Rafael <rp.araujo2@gmail.com>
2021-03-14 13:15:20 -0300

    Alterações da página no dia 14/03/2021 e remoção do node_modules

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "001387af2de9270b6d33";
+/******/ 	var hotCurrentHash = "e40e3d493d45bebba1ba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
